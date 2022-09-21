7597a3df5ce300e94ff72766d1f83e0cad3259fd
ParticleG <particle_g@outlook.com>
2022-09-21 11:12:41 +0800

    - Configure `package.json`

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "h3c-dev-helper" is now active!');
+	console.log('"h3c-dev-helper" activated');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('h3c-dev-helper.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from H3C Dev Helper!');
 	});
@@ -22,5 +11,6 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	console.log('"h3c-dev-helper" deactivated');
+}
